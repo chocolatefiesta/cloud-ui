1fe97ac0bb4333d41dbaf7fd144504012cd88852
Roman Fasakhov <fasashen@gmail.com>
2021-02-10 15:17:10 +0300

    Analytics

--- src/components/App/firebase.js
+++ src/components/App/firebase.js
@@ -9,7 +9,8 @@ export const firebaseConfig = {
     projectId: "chocolate-fiesta-cloud",
     storageBucket: "chocolate-fiesta-cloud.appspot.com",
     messagingSenderId: "419100703725",
-    appId: "1:419100703725:web:42a642a456befea15045e4"
+    appId: "1:419100703725:web:42a642a456befea15045e4",
+    measurementId: "G-FRZNRK641V"
 };
 
 export const storageUserFolder = 'user';
@@ -33,6 +34,7 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const storage = firebase.storage();
+export const analytics = firebase.analytics();
 
 export const firestoreUserDrawingDocument = (uid) => {
   return db.collection(firestoreUserDrawingCollection).doc(uid)
