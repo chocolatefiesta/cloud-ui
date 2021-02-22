9fccebba4f573d9acc787cfc9a43b98642fa59e2
Roman Fasakhov <fasashen@gmail.com>
2021-02-22 11:48:49 +0300

    Sentry Integration

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
+Sentry.init({
+  dsn: "https://671da45dc307486090d441b0ff074028@o528559.ingest.sentry.io/5646853",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
